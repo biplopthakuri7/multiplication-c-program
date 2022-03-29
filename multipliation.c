1dc76d85f1bea994e42781dc0dbcafa4c04b3398
biplopthakuri7 <biplopthakuree1234@gmail.com>
2022-03-29 12:01:58 +0545

    c programmultiplicatiion complete

---
+++ multipliation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,mul;
+printf("Enter the two number for multiplication");
+scanf("%f%f",&a,&b);
+mul=a*b;
+printf("the multiplication is:%f",mul);
+return 0;
+}
