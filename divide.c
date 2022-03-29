9e88fd236b27da41c61c77afbe6ebc59bf9f2544
biplopthakuri7 <biplopthakuree1234@gmail.com>
2022-03-29 16:39:07 +0545

    file divide is updated

--- divide.c
+++ divide.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter two integers: ");
     scanf("%f %f", &number1, &number2);
 
-    div = number1 % number2;      
+    div = number1 / number2;      
     
     printf("%f % %f = %f", number1, number2, div);
     return 0;
